ee5c3b7440091e0ad16e12d81b030d4ac115000c
dajicidea <dajicideasw@outlook.com>
2019-11-02 08:57:13 +0100

    Error handler

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -47,7 +47,6 @@ namespace DatingApp.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
-            throw new Exception("Haris test");
             var userFromRepo = await _repository.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
             if(userFromRepo == null)
